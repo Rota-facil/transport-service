ba749751b160abfbfe7f9c3546fe54c69c615f70
Thauã Gabriel <tagashi.dev@gmail.com>
2026-05-29 12:49:20 -0300

    feat: add global exception handler

--- src/main/java/com/rota/facil/transport_service/domain/exceptions/InvalidTimeToInitTrip.java
+++
@@ -1,9 +0,0 @@
-package com.rota.facil.transport_service.domain.exceptions;
-
-import org.springframework.beans.factory.annotation.Value;
-
-public class InvalidTimeToInitTrip extends RuntimeException {
-    public InvalidTimeToInitTrip(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/com/rota/facil/transport_service/domain/exceptions/InvalidTimeToInitTripException.java
@@ -0,0 +1,7 @@
+package com.rota.facil.transport_service.domain.exceptions;
+
+public class InvalidTimeToInitTripException extends RuntimeException {
+    public InvalidTimeToInitTripException(String message) {
+        super(message);
+    }
+}
