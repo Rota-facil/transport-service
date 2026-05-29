d23e61070d5579ceb405c3aaa610ab9025f2d9d7
Thauã Gabriel <tagashi.dev@gmail.com>
2026-05-29 12:58:47 -0300

    feat: add exception annotation

--- src/main/java/com/rota/facil/transport_service/domain/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/rota/facil/transport_service/domain/exceptions/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
@@ -12,62 +13,77 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler
     public ResponseEntity<Object> boardPointNotFoundException(BoardPointNotFoundException ex, HttpServletRequest request) {
         return this.resolveExceptions(HttpStatus.NOT_FOUND, request, ex);
     }
 
+    @ExceptionHandler
     public ResponseEntity<Object> busNotFoundException(BusNotFoundException ex, HttpServletRequest request) {
         return this.resolveExceptions(HttpStatus.NOT_FOUND, request, ex);
     }
 
+    @ExceptionHandler
     public ResponseEntity<Object> institutionNotFoundException(InstitutionNotFoundException ex, HttpServletRequest request) {
         return this.resolveExceptions(HttpStatus.NOT_FOUND, request, ex);
     }
 
+    @ExceptionHandler
     public ResponseEntity<Object> invalidTimeToInitTripException(InvalidTimeToInitTripException ex, HttpServletRequest request) {
         return this.resolveExceptions(HttpStatus.BAD_REQUEST, request, ex);
     }
 
+    @ExceptionHandler
     public ResponseEntity<Object> invalidTypeUserException(InvalidTypeUserException ex, HttpServletRequest request) {
         return this.resolveExceptions(HttpStatus.BAD_REQUEST, request, ex);
     }
 
+    @ExceptionHandler
     public ResponseEntity<Object> maxBusCapacityException(MaxBusCapacityException ex, HttpServletRequest request) {
         return this.resolveExceptions(HttpStatus.BAD_REQUEST, request, ex);
     }
 
+    @ExceptionHandler
     public ResponseEntity<Object> routeNotFoundException(RouteNotFoundException ex, HttpServletRequest request) {
         return this.resolveExceptions(HttpStatus.NOT_FOUND, request, ex);
     }
 
+    @ExceptionHandler
     public ResponseEntity<Object> tripAlreadyCancelledException(TripAlreadyCancelledException ex, HttpServletRequest request) {
         return this.resolveExceptions(HttpStatus.BAD_REQUEST, request, ex);
     }
 
+    @ExceptionHandler
     public ResponseEntity<Object> tripAlreadyStatedOrCancelledException(TripAlreadyStatedOrCancelledException ex, HttpServletRequest request) {
         return this.resolveExceptions(HttpStatus.BAD_REQUEST, request, ex);
     }
 
+    @ExceptionHandler
     public ResponseEntity<Object> tripNotFoundException(TripNotFoundException ex, HttpServletRequest request) {
         return this.resolveExceptions(HttpStatus.BAD_REQUEST, request, ex);
     }
 
+    @ExceptionHandler
     public ResponseEntity<Object> tripStartedStillNotFinishYetException(TripStartedStillNotFinishYetException ex, HttpServletRequest request) {
         return this.resolveExceptions(HttpStatus.BAD_REQUEST, request, ex);
     }
 
+    @ExceptionHandler
     public ResponseEntity<Object> tripStatusAlreadyRegisteredException(TripStatusAlreadyRegisteredException ex, HttpServletRequest request) {
         return this.resolveExceptions(HttpStatus.BAD_REQUEST, request, ex);
     }
 
+    @ExceptionHandler
     public ResponseEntity<Object> tripUserNotFoundException(TripUserNotFoundException ex, HttpServletRequest request) {
         return this.resolveExceptions(HttpStatus.NOT_FOUND, request, ex);
     }
 
+    @ExceptionHandler
     public ResponseEntity<Object> userAlreadyInTripException(UserAlreadyInTripException ex, HttpServletRequest request) {
         return this.resolveExceptions(HttpStatus.BAD_REQUEST, request, ex);
     }
 
+    @ExceptionHandler
     public ResponseEntity<Object> userNotFoundException(UserNotFoundException ex, HttpServletRequest request) {
         return this.resolveExceptions(HttpStatus.NOT_FOUND, request, ex);
     }
