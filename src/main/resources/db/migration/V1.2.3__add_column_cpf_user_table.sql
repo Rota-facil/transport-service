44b5b1ed4ea0b924a698cd76b3af0f73eaaacadc
Thauã Gabriel <tagashi.dev@gmail.com>
2026-07-06 06:02:11 -0300

    fix: fix migration

--- src/main/resources/db/migration/V1.2.3__add_column_cpf_user_table.sql
+++ src/main/resources/db/migration/V1.2.3__add_column_cpf_user_table.sql
@@ -3,15 +3,15 @@ ALTER TABLE users_tb
 
 WITH numbered AS (
     SELECT
-        id,
-        ROW_NUMBER() OVER (ORDER BY id) AS rn
+        user_id,
+        ROW_NUMBER() OVER (ORDER BY user_id) AS rn
     FROM users_tb
     WHERE cpf IS NULL
 )
 UPDATE users_tb u
 SET cpf = LPAD(numbered.rn::text, 11, '0')
     FROM numbered
-WHERE u.id = numbered.id;
+WHERE u.user_id = numbered.id;
 
 ALTER TABLE users_tb
     ADD CONSTRAINT u_cpf UNIQUE (cpf);
